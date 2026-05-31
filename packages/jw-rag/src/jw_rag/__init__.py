b47d2d2490b5dead2a547b7f85af5c294e9a1575
eliascipre <elimoralsmendox@gmail.com>
2026-05-31 08:28:15 -0600

    feat(jw-rag): re-export EmbedProvider/Reranker/factories from top-level

--- packages/jw-rag/src/jw_rag/__init__.py
+++ packages/jw-rag/src/jw_rag/__init__.py
@@ -2,19 +2,36 @@
 
 Public surface:
     from jw_rag import VectorStore, FakeEmbedder, Chunk, chunk_paragraphs
+    from jw_rag import EmbedProvider, Reranker, get_default_embedder, get_default_reranker
 """
 
 from jw_rag.chunker import Chunk, chunk_paragraphs
 from jw_rag.embed import Embedder, FakeEmbedder
+from jw_rag.embed_providers import (
+    EmbedProvider,
+    get_default_embedder,
+    list_available_embedders,
+)
+from jw_rag.rerank import (
+    Reranker,
+    get_default_reranker,
+    list_available_rerankers,
+)
 from jw_rag.store import SearchHit, VectorStore
 
 __version__ = "0.1.0"
 
 __all__ = [
     "Chunk",
+    "EmbedProvider",
     "Embedder",
     "FakeEmbedder",
+    "Reranker",
     "SearchHit",
     "VectorStore",
     "chunk_paragraphs",
+    "get_default_embedder",
+    "get_default_reranker",
+    "list_available_embedders",
+    "list_available_rerankers",
 ]
